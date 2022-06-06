6b7a3f084f8c8d28cdb6cfdd3ad65ee6a3a841ef
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-06-06 19:44:10 +0200

    Solved exercise 1 & 2

---
+++ index.js
@@ -0,0 +1,32 @@
+const book = document.querySelector("#bookSection");
+
+function searchDeezer() {
+  fetch("https://striveschool-api.herokuapp.com/books")
+    .then((response) => response.json())
+    .then((dataBook) => {
+      console.log(dataBook);
+
+      book.innerHTML = dataBook
+        .map(
+          (
+            books
+          ) => `<div class="card" style="height: 100%; background-color:#10171F">
+        <img src="${books.img}" class="card-img-top img-fluid w-100" alt="...">
+        <div class="card-body" style="height: 100%">
+        <h5 class="card-title">${books.title}</h5>
+        <p class="card-text">${books.category}</p>
+        <p class="card-text">$${books.price}</p>
+        </div>
+        </div>`
+        )
+        .join(" ");
+    })
+    .catch((err) => {
+      console.log("rejected");
+      console.log(err);
+    });
+}
+
+window.onload = function () {
+  searchDeezer();
+};
