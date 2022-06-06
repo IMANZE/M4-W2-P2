d327bb88b5c6897ebcc83a573df93f8ca73f3555
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-06-06 22:31:15 +0200

    card format updated

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ function searchDeezer() {
 
       book.innerHTML = dataBook
         .map(
-          (
-            books
-          ) => `<div class="card niceBooks" style="height: 100%; background-color:#10171F">
+          (books) => `<div class="col-md-4 pb-5 niceBooks">
+          <div class="card  mb-4 shadow-sm h-100">
+          <div class="card h-100">
+          <div class="card" style="height: 100%; background-color:#10171F">
         <img src="${books.img}" class="card-img-top img-fluid w-100" alt="...">
-        <div class="card-body" style="height: 100%">
+        <div class="card-body">
         <h5 class="card-title">${books.title}</h5>
         <p class="card-text">${books.category}</p>
         <p class="card-text">$${books.price}</p>
         <button type="button" class="btn btn-dark">Add to cart</button>
         <button type="button" class="btn btn-dark skip">Skip</button>
+        </div>
+        </div>
+        </div>
         </div>
         </div>`
         )
@@ -37,34 +41,42 @@ function searchDeezer() {
 
 function bookDisplay(bookContent) {
   const search = document.querySelector("#displaySection");
-  const searchEnquiry = bookContent.target.value
+  const searchEnquiry = bookContent.target.value;
   fetch("https://striveschool-api.herokuapp.com/books")
     .then((response) => response.json())
     .then((dataBook) => {
       console.log(dataBook);
-      search.innerHTML = dataBook.filter((books)=>books.title
-      .toLowerCase()
-      .includes(searchEnquiry.toLowerCase())).map((books)=> `<div class="card niceBooks" style="height: 100%; background-color:#10171F">
-      <img src="${books.img}" class="card-img-top img-fluid w-100" alt="...">
-      <div class="card-body" style="height: 100%">
-      <h5 class="card-title">${books.title}</h5>
-      <p class="card-text">${books.category}</p>
-      <p class="card-text">$${books.price}</p>
-      <button type="button" class="btn btn-dark">Add to cart</button>
-      <button type="button" class="btn btn-dark skip">Skip</button>
-      </div>
-      </div>`
-      )
-      .join(" ")
-     
+      search.innerHTML = dataBook
+        .filter((books) =>
+          books.title.toLowerCase().includes(searchEnquiry.toLowerCase())
+        )
+        .map(
+          (books) => `<div class="col-md-4 pb-5 niceBooks">
+      <div class="card  mb-4 shadow-sm h-100">
+      <div class="card h-100">
+      <div class="card" style="height: 100%; background-color:#10171F">
+    <img src="${books.img}" class="card-img-top img-fluid w-100" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${books.title}</h5>
+    <p class="card-text">${books.category}</p>
+    <p class="card-text">$${books.price}</p>
+    <button type="button" class="btn btn-dark">Add to cart</button>
+    <button type="button" class="btn btn-dark skip">Skip</button>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>`
+        )
+        .join(" ");
     })
-        
+
     .catch((err) => {
-        console.log("rejected");
-        console.log(err);
-      });  
-};
+      console.log("rejected");
+      console.log(err);
+    });
+}
 window.onload = function () {
   searchDeezer();
- document.querySelector(".inputField").addEventListener('change', bookDisplay)
+  document.querySelector(".inputField").addEventListener("change", bookDisplay);
 };
